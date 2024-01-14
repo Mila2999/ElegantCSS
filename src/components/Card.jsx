abc043508b54dec834aebcf18353597471f8c3d8
mila2999 <mila.savonik@gmail.com>
2024-01-14 21:26:34 +0200

    Created scc for first Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,17 @@ import classes from './Card.module.css';
 function Card() {
   return (
     <div className={classes.card}>
-      <div> + </div>
-      <p>Invite User to this Subscription</p>
-      <button>Invite User</button>
+      <div className={classes.circleContainer}>
+        <div className={classes.circle}>
+          <span className={classes.circleContent}>+</span>
+        </div>
+      </div>
+      <div>
+        <p>Invite User to this Subscription</p>
+      </div>
+      <div>
+        <button className={classes.button}>Invite User</button>
+      </div>
     </div>
   );
 }
